c8ce53a80585ff0417d6cfa28e2ed08facfb1fe8
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-20 13:32:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,8 +20,8 @@ class Song
   
   def self.genres(genre) 
     if @@genres.collect do |genre1, genre2| 
+      genre1 == genre2
       binding.pry
-      genre1 == genre2 
       end
     end 
   end 
