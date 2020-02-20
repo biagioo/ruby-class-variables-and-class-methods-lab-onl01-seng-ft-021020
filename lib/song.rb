9c36e6ca3b9a6a83084421447fb6fdf80003ec1c
Biagio   Mendolia <mendolia2328@gmail.com>
2020-02-20 14:35:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,14 +30,14 @@ class Song
   
   def self.genre_count 
     genre_count = {}
-    @@genres.each do |genre| 
+    @@genres.collect do |genre| 
       if genre_count[genre]
        genre_count[genre] += 1 
       else
         genre_count[genre] = 1 
       end 
     end
-    genre_count
+    # genre_count
   end 
   
   def self.artist_count 
